389bc0872e0954220298e4ca98d52898a647756c
SanksG1 <90807788+SanksG1@users.noreply.github.com>
2022-10-16 17:33:51 -0500

    Update Text_to_text.py

--- Source/Text_to_text.py
+++ Source/Text_to_text.py
@@ -1,6 +1,33 @@
+# from googletrans import Translator
+
+# translator = Translator()
+
+# result = translator.translate('Mikä on nimesi', src='fi', dest='fr')
+# print(result)
+
 from googletrans import Translator
+translator = Translator(service_urls=['translate.google.com','translate.google.co.kr',])
 
-translator = Translator()
+translator.translate('안녕하세요.')
 
-result = translator.translate('Mikä on nimesi', src='fi', dest='fr')
-print(result)
+# def translate_text(target, text):
+#     # """Translates text into the target language.
+
+#     # Target must be an ISO 639-1 language code.
+#     # See https://g.co/cloud/translate/v2/translate-reference#supported_languages
+#     # """
+#     import six
+#     from google.cloud import translate_v2 as translate
+
+#     translate_client = translate.Client()
+
+#     if isinstance(text, six.binary_type):
+#         text = text.decode("utf-8")
+
+#     # Text can also be a sequence of strings, in which case this method
+#     # will return a sequence of results for each text.
+#     result = translate_client.translate(text, target_language=target)
+
+#     print(u"Text: {}".format(result["input"]))
+#     print(u"Translation: {}".format(result["translatedText"]))
+#     print(u"Detected source language: {}".format(result["detectedSourceLanguage"]))
