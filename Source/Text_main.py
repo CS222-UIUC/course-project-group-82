6e1c49e269d7de8d005990bc30e0299ec929218f
Alexis Serrano <aserrano031@github.com>
2022-12-02 13:47:54 -0600

    updated main file and fixed text/textfile translation

--- Source/Text_main.py
+++ Source/Text_main.py
@@ -6,9 +6,12 @@ from googletrans import Translator
 import Text_to_text
 from OneDriveapi.Files_to_OneDrive import uploadToOneDrive
 import PyPDF2
+import os
 
 def inputToText():
+    os.remove("../OutputFiles/TranslatedText.txt")
     t = Text_to_text.TxtToTxt()
+    print("")
     text = input("Enter your text in: ") #Can the user type in spaces, try to see if it works
     print("")
     language_to_trans = input("Enter in the language that you want the text to be translated to: ") #Somehow need to ensure valid language entered
@@ -17,56 +20,39 @@ def inputToText():
     translator = Translator()
     translated_text = translator.translate(text,src = translator.detect(text).lang, dest = language_to_trans)
     print("Here is your translated text: " + translated_text.text)
+    print("")
     input_upload = input("Do you want to save this file to OneDrive? y - Yes, n - No ")
+    print("")
     if (input_upload == "y"):
         print("You will be redirected to sign in to Microsoft OneDrive.\n" + 
-        "Type the generated code and follow the instructions.\n" + "This is a one-time procedure.\n")
+        "Type the generated code and follow the instructions.\n" + "This is a one-time procedure.\n" + 
+        "If you already signed in to OneDrive. Type y.\n")
         response = input("Proceed?\n y - Yes, n - No: ")
+        print("")
         if (response == "y"):
             uploadToOneDrive("../OutputFiles/TranslatedText.txt")
             print("Successfully uploaded file!")
 
 def fileToText():
+    os.remove("../OutputFiles/TranslatedFile.txt")
     t = Text_to_text.TxtToTxt()
     file_to_trans = input("Type in the name of the file that you want to be translated: ")
+    print("")
     language_to_trans = input("Enter in the language that you want the text to be translated to: ")
+    print("")
     t.translateFile(file_to_trans, "../OutputFiles/TranslatedFile.txt", language_to_trans) #why underlined, we defined translateFile in TextTest.py
-    
+    input_upload = input("Do you want to save this file to OneDrive? y - Yes, n - No ")
+    print("")
+    if (input_upload == "y"):
+        print("You will be redirected to sign in to Microsoft OneDrive.\n" + 
+        "Type the generated code and follow the instructions.\n" + "This is a one-time procedure.\n" + 
+        "If you already signed in to OneDrive. Type y.\n")
+        response = input("Proceed?\n y - Yes, n - No: ")
+        print("")
+        if (response == "y"):
+            uploadToOneDrive("../OutputFiles/TranslatedFile.txt")
+            print("Successfully uploaded file!")
 
-#Archive Code
-# #Here we want to create some sort of interface with the user such that the user can type words and get translated as well send in file
-# intro = input("Hello There. Do you have anything you want to translate? y - Yes, n - No ")
-# print("") #getting an error when try to get user input
-# while (intro != "y" and intro != "n"):
-#     intro = input("Do you have anything you want to translate? y - Yes, n - No ")
-#     print("")
-#     #print("Error!, enter y for yes or n for no")
-# if (intro == "y"):
-#     input_translate = input("Do you want to type anything or do you have a file that you want to be translated to?\nt - type, txt - translate text file: ")
-#     print("")
-#     while (input_translate != "t" and input_translate != "txt"):
-#         input_translate = input("Do you want to type anything or do you have a file that you want to be translated to?\nt - type, txt - translate text file: ")
-#         print("")
-#     if (input_translate == "t"):
-#         text = input("Enter your text in: ") #Can the user type in spaces, try to see if it works
-#         print("")
-#         language_to_trans = input("Enter in the language that you want the text to be translated to: ") #Somehow need to ensure valid language entered
-#         print("")
-#         translateText(text, "TranslatedText.txt", language_to_trans) #Now translate it and output to file
-#         translator = Translator()
-#         translated_text = translator.translate(text,src = translator.detect(text).lang, dest = language_to_trans)
-#         print("Here is your translated text: " + translated_text.text)
-#         input_upload = input("Do you want to save this file to OneDrive? y - Yes, n - No ")
-#         if (input_upload == "y"):
-#             uploadToOneDrive("../Source/output_file.txt")
-#             print("Successfully uploaded file!")
-
-#     elif (input_translate == "txt"): #Suppose if user has a file to be translated to
-#         file_to_trans = input("Type in the name of the file that you want to be translated: ")
-#         language_to_trans = input("Enter in the language that you want the text to be translated to: ")
-#         translateFile(file_to_trans, "TranslatedFile.txt", language_to_trans) #why underlined, we defined translateFile in TextTest.py
-    
-    
     # else:  #Should we also give the option to the user for them to parse in pdf file? (useful if they have a certain document)
     #     #Note the pdf needs to be within the same folder
     #     pdf_ = input("Type in the name of pdf. Remeber to enter .pdf at end of file name: ") #Need to ensure valid pdf is entered
